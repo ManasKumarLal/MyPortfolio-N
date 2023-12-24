64613a6a320f45f67b287b45c34e4f01d4549f84
ManasKumarLal <skillszone100@gmail.com>
2023-12-24 18:00:03 +0530

    contact form

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -11,36 +11,34 @@ const Contact = () => {
     return (
         <>
             <div id='contact' className="contact_container" >
-                <h1>Contact me!!</h1>
-                <figure>
-                    <div className="wrapper">
+                <div className="wrapper">
 
-                        <div className="personal_details w-[30%]">
-                            <SingleDetail
-                                detail="7261876702"
-                                logo={<MdCall className='single_detail_logo' />}
-                            />
-                            <SingleDetail
-                                detail="skillszone100@gmail.com"
-                                logo={<MdEmail className='single_detail_logo' />}
-                            />
-                            <SingleDetail
-                                detail="Bhagalpur, Bihar, india"
-                                logo={<MdLocationPin className='single_detail_logo' />}
-                            />
-                            <div className="icons">
-                                <Icons/>
-                            </div>
+                    <div className="personal_details w-[30%]">
+                        <h1>Contact me!!</h1>
+                        <SingleDetail
+                            detail="7261876702"
+                            logo={<MdCall className='single_detail_logo' />}
+                        />
+                        <SingleDetail
+                            detail="skillszone100@gmail.com"
+                            logo={<MdEmail className='single_detail_logo' />}
+                        />
+                        <SingleDetail
+                            detail="Bhagalpur, Bihar, india"
+                            logo={<MdLocationPin className='single_detail_logo' />}
+                        />
+                        <div className="icons">
+                            <Icons />
                         </div>
-
-                        <div className='line'></div>
-
-                        <div className='w-[30%]'>
-                            <Form />
-                        </div>
-
                     </div>
-                </figure>
+
+                    <div className='line'></div>
+
+                    <div className='w-[30%]'>
+                        <Form />
+                    </div>
+
+                </div>
             </div>
         </>
     )
